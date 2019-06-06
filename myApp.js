b13191a6c7cb06e1efdca9776d6fb68612931225
SL477 <fishwickt@gmail.com>
2019-06-06 22:20:46 +0100

    challenges 1-7

--- myApp.js
+++ myApp.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(challenge7);
+function challenge7(req, res, next){
+	console.log(req.method + " " + req.path + " - " + req.ip);
+	next();
+}
 
 // --> 11)  Mount the body-parser middleware  here
 
@@ -12,23 +16,44 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+//app.HttpMethod(path, handler);
+/*app.get("/",challenge2);
+function challenge2(challenge2, result){
+	result.send('Hello Express');
+}*/
 
 /** 3) Serve an HTML file */
-
+//node global variable to get the path of where it is running __dirname
+app.get("/", challenge3);
+function challenge3(challenge3, result){
+	result.sendFile(__dirname + "/views/index.html");
+}
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+//GET retrieves data without modifying anything
+app.get("/json", challenge5);
+function challenge5(challenge5, result){
+	var hi = "Hello json";
+	//var environment = process.env.NODE_ENV || 'development';
+	//require('dotenv').load();
+	
+	//console.log(process.env.MESSAGE_STYLE);
+	if (process.env.MESSAGE_STYLE == "uppercase"){
+		hi = hi.toUpperCase();
+	}
+	result.json({"message": hi});
+}
 
 /** 6) Use the .env file to configure the app */
- 
+ //.env is a hidden file for storing API keys. Don't use quotes or spaces
+ //to get the env variables you use process.env.VAR_NAME
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
-
+//for middleware you can use app.use for everything or app.post for post requests (same for get, delete, put, ...)
 
 /** 8) Chaining middleware. A Time server */
 
